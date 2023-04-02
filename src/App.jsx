7a0d3653628c01799c376cb640d3e099caccb7b6
Nishant Mehta <nishme0708@gmail.com>
2023-04-02 16:53:33 +0530

    Add code changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,41 @@
+import { useEffect, useState } from 'react';
+import Loading from './Loading';
+import Tours from './Tours';
+
 const url = 'https://course-api.com/react-tours-project';
 
 const App = () => {
-  return <h2>Tours Starter</h2>;
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+  const removeTour = (id) => {
+    setTours(tours.filter((tour) => tour.id != id));
+  };
+  const fetchTours = () => {
+    setLoading(true);
+    return fetch(url)
+      .then((resp) => resp.json())
+      .then((data) => {
+        setTours(data);
+        setLoading(false);
+      });
+  };
+  const handleReload = () => {
+    fetchTours();
+  };
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
+  return (
+    <main>
+      {loading && <Loading></Loading>}
+      {!loading && <Tours tours={tours} removeTour={removeTour}></Tours>}
+      {tours.length == 0 && !loading && (
+        <button className='btn btn-block' onClick={handleReload}>
+          Reload
+        </button>
+      )}
+    </main>
+  );
 };
 export default App;
