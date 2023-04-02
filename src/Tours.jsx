a69a9921eee7710ed03b450abe6b596f921c1d28
Nishant Mehta <nishme0708@gmail.com>
2023-04-02 16:53:10 +0530

    Add Tour component

---
+++ src/Tours.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react';
+import Tour from './Tour';
+const Tours = ({ tours, removeTour }) => {
+  return (
+    <section>
+      <div className='title'>
+        <h2>Our Tours</h2>
+        <div className='title-underline'></div>
+      </div>
+      <div className='tours'>
+        {tours.map((tour) => {
+          return <Tour key={tour.id} {...tour} removeTour={removeTour}></Tour>;
+        })}
+      </div>{' '}
+    </section>
+  );
+};
+export default Tours;
